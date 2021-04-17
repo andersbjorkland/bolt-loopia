d36413bf2589045b22253b60592838ac2b3a3dc8
Anders Björkland <git@andersbjorkland.online>
2021-04-17 21:12:40 +0200

    updated deploy recipe

--- deploy.php
+++ deploy.php
@@ -45,5 +45,5 @@ after('deploy:failed', 'deploy:unlock');
 
 // Migrate database before symlink new release.
 
-before('deploy:symlink', 'database:migrate');
+//before('deploy:symlink', 'database:migrate');
 
